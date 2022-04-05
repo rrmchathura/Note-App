39724b0b27fff554631a4a4c64b961cb1f608811
Chathura Sanjaya <49064590+rrmchathura@users.noreply.github.com>
2022-04-06 00:29:07 +0530

    noteAdd

--- app/src/main/java/com/rrmchathura/note_app/MainActivity.java
+++ app/src/main/java/com/rrmchathura/note_app/MainActivity.java
@@ -1,14 +1,84 @@
 package com.rrmchathura.note_app;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.rrmchathura.note_app.Adapters.NotesListAdapter;
+import com.rrmchathura.note_app.Database.RoomDB;
+import com.rrmchathura.note_app.Models.Notes;
+import com.rrmchathura.note_app.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    NotesListAdapter notesListAdapter;
+    List<Notes> notes = new ArrayList<>();
+    RoomDB database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        database = RoomDB.getInstance(this);
+        notes = database.mainDAO().getAll();
+
+        updateRecycle(notes);
+
+        binding.fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,NotesTakenActivity.class);
+                startActivityForResult(intent,101);
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode==101){
+            if (resultCode== Activity.RESULT_OK){
+                Notes new_notes = (Notes) data.getSerializableExtra("note");
+                database.mainDAO().insert(new_notes);
+                notes.clear();
+                notes.addAll(database.mainDAO().getAll());
+                notesListAdapter.notifyDataSetChanged();
+            }
+        }
+    }
+
+    private void updateRecycle(List<Notes> notes) {
+        binding.recyclerHome.setHasFixedSize(true);
+        binding.recyclerHome.setLayoutManager(new StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL));
+        notesListAdapter = new NotesListAdapter(MainActivity.this,notes,notesClickListener);
+        binding.recyclerHome.setAdapter(notesListAdapter);
+    }
+
+    private final NotesClickListener notesClickListener = new NotesClickListener() {
+        @Override
+        public void onClick(Notes notes) {
+
+        }
+
+        @Override
+        public void onLongClick(Notes notes, CardView cardView) {
+
+        }
+    };
 }
--- app/src/main/java/com/rrmchathura/note_app/NotesTakenActivity.java
+++ app/src/main/java/com/rrmchathura/note_app/NotesTakenActivity.java
@@ -2,13 +2,53 @@ package com.rrmchathura.note_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.rrmchathura.note_app.Models.Notes;
+import com.rrmchathura.note_app.databinding.ActivityNotesTakenBinding;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class NotesTakenActivity extends AppCompatActivity {
+
+    ActivityNotesTakenBinding binding;
+    Notes notes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notes_taken);
+        binding = ActivityNotesTakenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.imageViewSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String title = binding.editTextTitle.getText().toString();
+                String description = binding.editTextNotes.getText().toString();
+
+                if (description.isEmpty()){
+                    binding.editTextNotes.setError("Please add some notes!");
+                    return;
+                }
+                else {
+                    SimpleDateFormat formatter = new SimpleDateFormat("EEE,d MMM yyyy HH:mm a");
+                    Date date = new Date();
+
+                    notes = new Notes();
+                    notes.setTitle(title);
+                    notes.setNotes(description);
+                    notes.setDate(formatter.format(date));
+
+                    Intent intent = new Intent();
+                    intent.putExtra("note",notes);
+                    setResult(Activity.RESULT_OK,intent);
+                    finish();
+                }
+            }
+        });
     }
 }
