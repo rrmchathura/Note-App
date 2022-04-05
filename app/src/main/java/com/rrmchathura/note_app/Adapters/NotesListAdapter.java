99f8496a18840a0ac5012b3fcd1bd23213c33304
Chathura Sanjaya <49064590+rrmchathura@users.noreply.github.com>
2022-04-05 20:34:03 +0530

    noteListAdapter

---
+++ app/src/main/java/com/rrmchathura/note_app/Adapters/NotesListAdapter.java
@@ -0,0 +1,116 @@
+package com.rrmchathura.note_app.Adapters;
+
+import android.content.Context;
+import android.os.Build;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.rrmchathura.note_app.Models.Notes;
+import com.rrmchathura.note_app.NotesClickListener;
+import com.rrmchathura.note_app.R;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class NotesListAdapter extends RecyclerView.Adapter<NotesViewHolder> {
+
+    Context context;
+    List<Notes> notesList;
+    NotesClickListener listener;
+
+    public NotesListAdapter(Context context, List<Notes> notesList, NotesClickListener listener) {
+        this.context = context;
+        this.notesList = notesList;
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public NotesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new NotesViewHolder(LayoutInflater.from(context).inflate(R.layout.notes_list, parent, false));
+
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onBindViewHolder(@NonNull NotesViewHolder holder, int position) {
+        holder.textView_title.setText(notesList.get(position).getTitle());
+        holder.textView_title.setSelected(true);
+
+        holder.textView_notes.setText(notesList.get(position).getNotes());
+
+        holder.textView_date.setText(notesList.get(position).getDate());
+        holder.textView_date.setSelected(true);
+
+        if (notesList.get(position).isPinned()){
+            holder.imageView_pin.setImageResource(R.drawable.pin);
+        }
+        else {
+            holder.imageView_pin.setImageResource(0);
+        }
+
+        int color_code = getRandomColor();
+        holder.notes_container.setCardBackgroundColor(holder.itemView.getResources().getColor(color_code,null));
+
+        holder.notes_container.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.onClick(notesList.get(holder.getAdapterPosition()));
+            }
+        });
+
+        holder.notes_container.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                listener.onLongClick(notesList.get(holder.getAdapterPosition()),holder.notes_container);
+                return true;
+            }
+        });
+    }
+
+    private int getRandomColor(){
+        List<Integer> colorCode = new ArrayList<>();
+
+        colorCode.add(R.color.color1);
+        colorCode.add(R.color.color2);
+        colorCode.add(R.color.color3);
+        colorCode.add(R.color.color4);
+        colorCode.add(R.color.color5);
+
+        Random random = new Random();
+        int random_color = random.nextInt(colorCode.size());
+        return random_color;
+    }
+
+    @Override
+    public int getItemCount() {
+        return notesList.size();
+    }
+}
+
+class NotesViewHolder extends RecyclerView.ViewHolder {
+
+    CardView notes_container;
+    TextView textView_title, textView_notes, textView_date;
+    ImageView imageView_pin;
+
+    public NotesViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        notes_container = itemView.findViewById(R.id.notes_container);
+        textView_title = itemView.findViewById(R.id.textView_title);
+        textView_notes = itemView.findViewById(R.id.textView_notes);
+        textView_date = itemView.findViewById(R.id.textView_date);
+        imageView_pin = itemView.findViewById(R.id.imageView_pin);
+    }
+}
